c6f1906ed448c3cae503829dc21ec60805634945
Julian Laffey <julianlaffey@gmail.com>
2015-07-04 23:45:46 +1000

    Dynamic text update on poll sliders

--- resources/public/js/place.js
+++ resources/public/js/place.js
@@ -1,8 +1,39 @@
 
+var responseLabels = [
+	"None",
+	"A little",
+	"Somewhat",
+	"Very",
+	"Extremely"];
+
+
+/*
+	TODO
+	Code below relies on elements being next to each other.
+	Needs less brittle solution.
+*/
+
+
+
+// on checkbox select
 $('input[type="checkbox"]').change(function(e) {
 	if (e.target.checked) {
 		$(this).next().attr('disabled', 'disabled');
+		$(this).next().next().css('visibility', 'hidden');
 	} else {
 		$(this).next().removeAttr('disabled');
+		$(this).next().next().css('visibility', 'visible');
 	}
 });
+
+
+// set labels
+$('p.label').text(responseLabels[2]);
+
+
+// on slider change
+$('input[type="range"]').on("input change", function(e) {
+	$(this).next().text(responseLabels[e.target.value]);
+});
+
+
